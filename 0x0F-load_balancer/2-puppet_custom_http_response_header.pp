78c4d9d82385b7f51d46e17fff641ed453b631e5
Jong9106 <juank_hr@hotmail.com>
2021-04-12 23:15:42 -0500

    create a new machine ubuntu with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+# Install Nginx web server
+
+exec { 'update server':
+  command  => 'apt-get update',
+  provider => 'shell',
+}
+->
+package { 'nginx':
+  ensure   => installed,
+}
+->
+file_line { 'custom HTTP header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+service { 'nginx':
+  ensure  => running,
+}
