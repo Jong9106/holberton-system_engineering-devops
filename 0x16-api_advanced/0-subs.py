7b32d0b99ec737ea9ca14259b419b9b71f506a59
Jong9106 <juank_hr@hotmail.com>
2021-05-17 19:14:16 -0500

    Pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,12 @@ Module that create a new functions
 import requests
 import json
 
-def number_of_subscribers(subreddit):
 
-    resp = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit))
+def number_of_subscribers(subreddit):
+    """ Function to get the numbers os subscribers """
+
+    url = 'https://www.reddit.com'
+    resp = requests.get('{}/r/{}/about.json'.format(url, subreddit))
     rspjson = resp.json()
     data = rspjson.get("data")
     return int(data.get('subscribers'))
