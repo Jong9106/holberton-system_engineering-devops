1112b82969cbeed1c40c3ee0b0fd8eff68eaa792
Jong9106 <juank_hr@hotmail.com>
2021-05-17 19:30:34 -0500

    pep8 style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,8 @@ def number_of_subscribers(subreddit):
 
     agent = {"User-Agent": 'Juan/007'}
     url = 'https://www.reddit.com'
-    resp = requests.get('{}/r/{}/about.json'.format(url, subreddit), headers=agent, allow_redirects=False)
+    resp = (requests.get('{}/r/{}/about.json'.format(url, subreddit),
+            headers=agent, allow_redirects=False))
     if resp.status_code != 200:
         return 0
     rspjson = resp.json()
